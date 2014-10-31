07dcaedf0d2f35d5635cc5fb6c1e04ccd5c975b8
migalkin <mikhgalkin@gmail.com>
2014-10-31 15:59:35 +0100

    templateMapping.js has been re-written visualizationController.js can now receive correct mappings 'style' color option for charts excluded

--- frontend/app/scripts/controllers/templateMapping.js
+++ frontend/app/scripts/controllers/templateMapping.js
@@ -3,40 +3,93 @@ function templateMapping(editObject) {
     //or the JSON with changed template data, i.e. {tuningOptions:{height:500}}
 
     var tuningOptions = null;
-    var structureOptions = null;
+        var structureOptions = null;
+        var resultMapping = {
+            mappingsTuning: [],
+            mappingsStructure: [],
+            configuration: {}
+        };
+    
+        //Assuming there is a baseofmappings {option: template}
+        var mapDB = {
+            "dimensions": "dimension-area",
+            "width": "tuningWidth",
+            "height":"tuningHeight",
+            "background_color": "tuningBckGrndColor",
+            "hLabel":"tuningHorizontalLabel",
+            "vLabel":"tuningVerticalLabel",
+            "numGridlinesHor":"tuningGridLinesHorizontal",
+            "numGridlinesVer":"tuningGridLinesVertical",
+            "ticks":"tuningTicks",
+            "widthPx":"tuningWidthPx",
+            "widthRatio":"tuningWidthRatio"
+        };
 
-    //retrieving the fields
-    if (editObject.hasOwnProperty("tuningOptions")) {
-        tuningOptions = editObject["tuningOptions"];
-    }
+        //retrieving the fields
+        if (editObject.hasOwnProperty("tuningOptions")) {
+            tuningOptions = editObject["tuningOptions"];
+        }
 
-    if (editObject.hasOwnProperty("structureOptions")) {
-        structureOptions = editObject["structureOptions"];
-    }
+        if (editObject.hasOwnProperty("structureOptions")) {
+            structureOptions = editObject["structureOptions"];
+        }
 
-    //invoking an appropriate template for a tuning parameter
-    if (tuningOptions !== null) {
-        for (var prop in tuningOptions) {
-            if (tuningOptions.hasOwnProperty(prop)) {
-                if (typeof(tuningOptions[prop]) !== 'object') {
-                    invokeTemplate(prop, tuningOptions[prop]);
-                } else {
-                    var axisOptions = tuningOptions[prop];
-                    for (var axisprop in axisOptions) {
-                        invokeTemplate(axisprop, axisOptions[axisprop]);
+        //invoking an appropriate template for a tuning parameter
+        if (tuningOptions !== null) {
+            for (var prop in tuningOptions) {
+                if (tuningOptions.hasOwnProperty(prop)) {
+                    if (typeof(tuningOptions[prop]) !== 'object') {
+                        resultMapping.mappingsTuning.push({
+                            template: mapDB[prop],
+                            options: {
+                                label: prop,
+                                value: tuningOptions[prop]
+                            }
+                        });
+                        //invokeTemplate(prop, tuningOptions[prop]);
+                    } else {
+                        var axisOptions = tuningOptions[prop];
+                        for (var axisprop in axisOptions) {
+                            resultMapping.mappingsTuning.push({
+                            template: mapDB[axisprop],
+                            options: {
+                                label: axisprop,
+                                value: axisOptions[axisprop]
+                            }
+                        });
+                            //invokeTemplate(axisprop, axisOptions[axisprop]);
+                        }
                     }
                 }
             }
         }
-    }
-
-    //invoking an appropriate template for a dimension parameter
-    if (structureOptions !== null) {
-        var dimensions = structureOptions['dimensions'];
-        for (var prop in dimensions) {
-            invokeTemplate(prop, dimensions[prop]);
+    
+    
+        //invoking an appropriate template for a dimension parameter
+        if (structureOptions !== null) {
+            var dimensions = structureOptions['dimensions'];
+        
+            /*building the configuration object
+             * { xAxis : [],
+             *   yAxis:  []
+             * }  
+             */
+            for (var prop in dimensions) {
+                resultMapping.configuration[prop]=dimensions[prop].values;
+            
+                resultMapping.mappingsStructure.push({
+                    template: mapDB['dimensions'],
+                    options: {
+                        label: prop,
+                        value: resultMapping.configuration[prop]
+                    }
+                });
+                //invokeTemplate(prop, dimensions[prop]);
+            }
         }
-    }
+    
+        return resultMapping;
+
 
 };
 
